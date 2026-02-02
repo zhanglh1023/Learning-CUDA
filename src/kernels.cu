edd2ea5e4462190772eb5c3fb24ff3163c2a368c
zhanglh <2461305435@qq.com>
2026-02-02 20:52:47 +0800

    flash_attn

--- src/kernels.cu
+++ src/kernels.cu
@@ -166,15 +166,15 @@ __global__ void flash_attn_kernel(T *q, T *k, T *v, T *o,
     float expf_pre = expf(m_pre - m);
     float expf_now = expf(m_now - m);
     float l = l_pre * expf_pre + l_now * expf_now;
-    float l_inv = 1.0 / l;
+    
     s_m[ty] = m;
     s_l[ty] = l;
     #pragma unroll
     for(size_t i = 0;i < dim;++i) {
       float value = sum * s_v[tx * dim + i];
       value = warp_reduce_sum<float>(value);
-      if(laneid == 0)
-        s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf_pre * l_pre + value * expf_now) * l_inv : 0.f;
+      if(laneid == 0) s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf_pre + value * expf_now) : 0.f;
+        //s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf_pre * l_pre + value * expf_now) * l_inv : 0.f;
     }
     // e^(x-m) / l * v
     k += Bc * kv_stride;
@@ -182,7 +182,9 @@ __global__ void flash_attn_kernel(T *q, T *k, T *v, T *o,
     kv_acc_len += Bc;
     __syncthreads();
   }
-  
+  for(size_t i = 0;i < dim;i++) {
+    if(laneid == 0) s_o[ty * dim + i] = s_o[ty * dim + i] / s_l[ty];
+  }
   #pragma unroll
   for(size_t i = tid;i < Br * dim;i += block_size) {
     int x = i % dim;
