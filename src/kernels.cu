13a33dae223c00e6621008ff6f7159669b2f67ac
zhanglh <2461305435@qq.com>
2026-02-02 19:51:55 +0800

    flash_attn

--- src/kernels.cu
+++ src/kernels.cu
@@ -171,7 +171,7 @@ __global__ void flash_attn_kernel(T *q, T *k, T *v, T *o,
       float value = sum * s_v[tx * dim + i];
       value = warp_reduce_sum<float>(value);
       if(laneid == 0)
-        s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf(m_pre - m) * l_pre + value * expf(m_now - m)) / l : 0.f;
+        s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf(m_pre - m) * l_pre / l) + value * expf(m_now - m) / l : 0.f;
     }
     // e^(x-m) / l * v
     k += Bc * kv_stride;
