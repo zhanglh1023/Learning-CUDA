89ed0fc70b6c4a47cd99112ec091f40f414e2ae5
zhanglh <2461305435@qq.com>
2026-02-02 20:43:07 +0800

    flash_attn

--- src/kernels.cu
+++ src/kernels.cu
@@ -153,26 +153,26 @@ __global__ void flash_attn_kernel(T *q, T *k, T *v, T *o,
       sum += static_cast<double>(s_q[ty * dim + i]) * static_cast<double>(s_k[tx * dim + i]);
     }
     sum *= scale;
-    double m_now = (((q_acc_len + ty < q_len) && (kv_acc_len + tx < kv_len)) && (!is_causal || q_acc_len + ty >= kv_acc_len + tx)) ? sum : -__FLT_MAX__;
-    m_now = warp_reduce_max<double>(m_now);
+    float m_now = (((q_acc_len + ty < q_len) && (kv_acc_len + tx < kv_len)) && (!is_causal || q_acc_len + ty >= kv_acc_len + tx)) ? sum : -__FLT_MAX__;
+    m_now = warp_reduce_max<float>(m_now);
     sum = (((q_acc_len + ty < q_len) && (kv_acc_len + tx < kv_len)) && (!is_causal || q_acc_len + ty >= kv_acc_len + tx)) ? exp(sum - m_now) : 0.f;
-    double l_now = sum;
-    l_now = warp_reduce_sum<double>(l_now);
+    float l_now = sum;
+    l_now = warp_reduce_sum<float>(l_now);
     
-    double m_pre = s_m[ty];
-    double l_pre = s_l[ty];
+    float m_pre = s_m[ty];
+    float l_pre = s_l[ty];
 
-    double m = max(m_pre, m_now);
-    double expf_pre = exp(m_pre - m);
-    double expf_now = exp(m_now - m);
-    double l = l_pre * expf_pre + l_now * expf_now + 1e-5;
-    double l_inv = 1.0 / l;
+    float m = fmaxf(m_pre, m_now);
+    float expf_pre = expf(m_pre - m);
+    float expf_now = expf(m_now - m);
+    float l = l_pre * expf_pre + l_now * expf_now;
+    float l_inv = 1.0 / l;
     s_m[ty] = m;
     s_l[ty] = l;
     #pragma unroll
     for(size_t i = 0;i < dim;++i) {
-      double value = sum * s_v[tx * dim + i];
-      value = warp_reduce_sum<double>(value);
+      float value = sum * s_v[tx * dim + i];
+      value = warp_reduce_sum<float>(value);
       if(laneid == 0)
         s_o[ty * dim + i] = (q_acc_len + ty < q_len) ? (s_o[ty * dim + i] * expf_pre * l_pre + value * expf_now) * l_inv : 0.f;
     }
