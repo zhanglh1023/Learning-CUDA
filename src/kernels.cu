e7fe1c88665466831bf003285c46a660c05e4ede
zhanglh <2461305435@qq.com>
2026-02-06 10:37:43 +0800

    update

--- src/kernels.cu
+++ src/kernels.cu
@@ -443,7 +443,7 @@ void flashAttention(const std::vector<T>& h_q, const std::vector<T>& h_k,
       //2048 
       constexpr int Br = 32;
       constexpr int Bc = 32;
-      constexpr int TM = 2;
+      constexpr int TM = 1;
       constexpr int TN = 8;
       constexpr int KBD = 16;
       constexpr int VBD = 8;
